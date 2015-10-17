d6d96aff86b9878b96b3c44f51bdbd221244b4a3
sepelka <sepelka@gmail.com>
2015-10-17 16:54:34 -0400

    menuv1

---
+++ SanMenuOptions.js
@@ -0,0 +1,9 @@
+var menuOptions = [{"name":"banking","URL":"", "subMenu":["moth":"Jan","ID":"n614466","LastName":"Sakowski","FirstName":"Alison","JobCode":"3077","PrevRole":"R1P2","Role":"R3P1"},
+	{"month":"Jan","ID":"n740673","LastName":"Sware","FirstName":"Richard","JobCode":"3051","PrevRole":"R1P2","Role":"R1P1"},
+	{"month":"Jan","ID":"n771497","LastName":"Banghart","FirstName":"Victoria","JobCode":"L03071","PrevRole":"R1P2","Role":"R2P1"},
+	{"month":"Jan","ID":"n619573","LastName":"Anderson","FirstName":"Nichole","JobCode":"3052","PrevRole":"R1P2","Role":"R1P1"},
+	{"month":"Jan","ID":"n778884","LastName":"Medina","FirstName":"Janine","JobCode":"3077","PrevRole":"R1P2","Role":"R3P1"},
+	{"month":"Jan","ID":"n609539","LastName":"Genovese","FirstName":"Judith","JobCode":"3071","PrevRole":"R10P1","Role":"R2P1"},
+	{"month":"Jan","ID":"n611979","LastName":"Small","FirstName":"Patricia","JobCode":"3077","PrevRole":"R10P1","Role":"R3P1"},
+	{"month":"Jan","ID":"n612849","LastName":"Shaw","FirstName":"Jaqueline","JobCode":"3058","PrevRole":"R10P1","Role":"R3P2"}]}
+	{"month":"Feb","ID":"n769072","LastName":"Eckman","FirstName":"Andrew","JobCode":"3052","PrevRole":"R10P1","Role":"R1P1"},
